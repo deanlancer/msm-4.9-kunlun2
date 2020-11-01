b2c2e3990617c75ade1c6518354d9461b292161c
Deanlancer <deanlancer@gmail.com>
2020-11-01 09:24:58 +0300

    Revert "qcom: qpnp-smb5: Expose system temperature level properties"
    This reverts commit f54c71c9ecbece45423d196b600a58632d672b55.

--- drivers/power/supply/qcom/qpnp-smb5.c
+++ drivers/power/supply/qcom/qpnp-smb5.c
@@ -1265,10 +1265,6 @@ static enum power_supply_property smb5_batt_props[] = {
 	POWER_SUPPLY_PROP_CHARGE_FULL_DESIGN,
 	POWER_SUPPLY_PROP_TIME_TO_FULL_NOW,
 	POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE,
-
-	POWER_SUPPLY_PROP_BATTERY_CHARGING_ENABLED,
-	POWER_SUPPLY_PROP_SLOWLY_CHARGING,
-	POWER_SUPPLY_PROP_SYSTEM_TEMP_LEVEL,
 };
 
 #define ITERM_SCALING_FACTOR_PMI632	1525
@@ -1438,22 +1434,6 @@ static int smb5_batt_get_prop(struct power_supply *psy,
 	case POWER_SUPPLY_PROP_FCC_STEPPER_ENABLE:
 		val->intval = chg->fcc_stepper_enable;
 		break;
-
-	case POWER_SUPPLY_PROP_BATTERY_CHARGING_ENABLED:
-		rc = smblib_get_prop_battery_charging_enabled(chg, val);
-		break;
-	case POWER_SUPPLY_PROP_BATTERY_CHARGING_LIMITED:
-		rc = smblib_get_prop_battery_charging_limited(chg, val);
-		break;
-	case POWER_SUPPLY_PROP_SLOWLY_CHARGING:
-		rc = smblib_get_prop_battery_slowly_charging(chg, val);
-		break;
-	case POWER_SUPPLY_PROP_SYSTEM_TEMP_LEVEL:
-		rc = smblib_get_prop_system_temp_level(chg, val);
-		break;
-	case POWER_SUPPLY_PROP_BQ_INPUT_SUSPEND:
-		rc = smblib_get_prop_battery_bq_input_suspend(chg, val);
-		break;
 	default:
 		pr_err("batt power supply prop %d not supported\n", psp);
 		return -EINVAL;
@@ -1550,13 +1530,6 @@ static int smb5_batt_set_prop(struct power_supply *psy,
 			vote(chg->chg_disable_votable, FORCE_RECHARGE_VOTER,
 					false, 0);
 		}
-		break;
-
-	case POWER_SUPPLY_PROP_SLOWLY_CHARGING:
-			rc = smblib_set_prop_battery_slowly_charging(chg, val);
-		break;
-	case POWER_SUPPLY_PROP_SYSTEM_TEMP_LEVEL:
-		rc = smblib_set_prop_system_temp_level(chg, val);
 		break;
 	default:
 		rc = -EINVAL;
